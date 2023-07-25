348dcdcd846675f2b642e438b58de0adbaf07c37
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-07-25 17:54:04 +0330

    write docs for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,10 @@ import { Subscription } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
 
   organizeData: any[];
+
+  /**
+  * Array to store subscriptions to observables.
+  */
   subscriptions: Subscription[] = [];
 
   constructor(private organizeService: OrganizeService) { }
@@ -19,6 +23,9 @@ export class AppComponent implements OnInit, OnDestroy {
     this.setSubscriptions();
   }
 
+  /**
+   * Sets up subscriptions to observables to receive updates when the organized data changes.
+   */
   setSubscriptions(): void {
     this.subscriptions.push(
       this.organizeService.getOrganizeData$().subscribe((data: any[]) => {
@@ -27,6 +34,11 @@ export class AppComponent implements OnInit, OnDestroy {
     );
   }
 
+  /**
+   * Event handler for the search input field.
+   * Triggers a search on the OrganizeService based on the provided search term.
+   * @param event The event object containing the search term entered by the user.
+   */
   onSearch(event: any): void {
     const searchTerm = event.target.value
     this.organizeService.searchOnData(searchTerm);
