6f9a7779511c8655131264f802ccaa6e8a2de5e0
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-07-25 17:40:17 +0330

    show data in main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { OrganizeService } from './service/organize.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'json-sorter';
+export class AppComponent implements OnInit, OnDestroy {
+
+  organizeData: any[];
+  subscriptions: Subscription[] = [];
+
+  constructor(private organizeService: OrganizeService) { }
+
+  ngOnInit(): void {
+    this.organizeService.loadData();
+    this.setSubscriptions();
+  }
+
+  setSubscriptions(): void {
+    this.subscriptions.push(
+      this.organizeService.getOrganizeData$().subscribe((data: any[]) => {
+        this.organizeData = data;
+      })
+    );
+  }
+
+  onSearch(event: any): void {
+    const searchTerm = event.target.value
+    this.organizeService.searchOnData(searchTerm);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
