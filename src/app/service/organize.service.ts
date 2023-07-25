ba7a736205f77e45566b41a2cfef7669dd7d43c0
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-07-25 17:52:26 +0330

    write some docs for orginize service

--- src/app/service/organize.service.ts
+++ src/app/service/organize.service.ts
@@ -13,7 +13,10 @@ export class OrganizeService {
 
   constructor(private http: HttpClient) { }
 
-
+  /**
+    * Fetches data from 'assets/data.json', organizes it into a nested tree structure, and emits the data to subscribers.
+    * This method is typically called to initialize the service with the data.
+    */
   loadData() {
     this.http.get('assets/data.json').subscribe((data: any) => {
       this.jsonData = data.data.list0;
@@ -23,6 +26,11 @@ export class OrganizeService {
     });
   }
 
+  /**
+  * Filters the nested tree view based on the provided search term and emits the filtered data to subscribers.
+  * If the search term is empty, the original nested tree view is emitted.
+  * @param searchTerm The search term to filter the tree view.
+  */
   searchOnData(searchTerm: string): void {
     if (!searchTerm.trim()) {
       this.organizeData$.next([...this.nestedView]);
@@ -31,12 +39,22 @@ export class OrganizeService {
     }
   }
 
-
+  /**
+    * Returns an observable that emits the organized nested tree data.
+    * Subscribers can receive updates whenever the tree data changes.
+    * @returns An observable of the organized nested tree data.
+    */
   getOrganizeData$(): Observable<any[]> {
     return this.organizeData$.asObservable();
   }
 
-
+  /**
+     * Organizes the data into a nested tree structure based on the parentId.
+     * @param data The array of data to organize into the nested tree structure.
+     * @param parentId The parentId to organize the data under.
+     * @returns The nested tree structure as an array.
+     * @private
+     */
   private organizeIntoNestedView(data: any[], parentId: number): any[] {
     return data
       .filter(item => item.parentId === parentId)
@@ -50,6 +68,13 @@ export class OrganizeService {
       });
   }
 
+  /**
+   * Filters the nested tree data based on the provided search term.
+   * @param data The array of data to filter.
+   * @param searchTerm The search term to filter the data with.
+   * @returns The filtered nested tree data as an array.
+   * @private
+   */
   private filterNestedView(data: any[], searchTerm: string): any[] {
     return data.reduce((acc, item) => {
       if (item.name.toLowerCase().includes(searchTerm)) {
