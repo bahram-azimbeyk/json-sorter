99bfb06b9584027dc77e34ff42c2c004914788ac
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-07-25 17:39:50 +0330

    declarat tree list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TreeListComponent } from './tree-list/tree-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
